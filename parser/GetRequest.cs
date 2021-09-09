4ad5e5ab17594d77a6a45d26f6ff5f5840bdce72
Рашид <@MAGICBOOK-PRO-1>
2021-09-09 21:41:19 +0300

    Добавьте файлы проекта.

---
+++ parser/GetRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SiteApi
+{
+    public class GetRequest
+    {
+        HttpWebRequest _request;
+        string _adress;
+
+        public string Response { get;set; }
+        public GetRequest(string adress)
+        {
+            _adress = adress;
+        }
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_adress);
+            _request.Method = "Get";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadLine();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ parser/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SiteApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "bzxKThZ8TWYxCyX6r5Juzh6omErxATBC";
+            var data = DateTime.Now.ToString("yyyy-MM-ddT00:00:00");
+            var start = "48.864716,2.349014";
+            var end = "45.75801,4.8000158";
+            var currency = "RUB";
+            var request = new GetRequest($"https://public-api.blablacar.com/api/v3/trips?from_coordinate={start}&to_coordinate={end}&locale=fr-FR&currency={currency}&start_date_local={data}&key={key}"); ; ; ; ;
+            request.Run();
+
+            var response = request.Response;
+
+            var json = JObject.Parse(response);
+            var trips = json["trips"];
+
+            foreach (var trip in trips)
+            {
+                var price= trip["price"];
+                var amount = price["amount"];
+
+                Console.WriteLine($"price={amount} руб.");
+            }
+        }
+    }
+}
